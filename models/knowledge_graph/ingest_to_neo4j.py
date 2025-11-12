275ace6e49b81f40821fb753c17b3d4751bc09a8
Rajan Dubey <rdrajan9452@gmail.com>
2025-11-12 23:45:40 +0530

    corrected credentials

--- models/knowledge_graph/ingest_to_neo4j.py
+++ models/knowledge_graph/ingest_to_neo4j.py
@@ -26,7 +26,7 @@ load_dotenv()
 
 NEO4J_URI = os.getenv("NEO4J_URI")
 NEO4J_USER = os.getenv("NEO4J_USER")
-NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD")
+NEO4J_PASS = os.getenv("NEO4J_PASS")
 TARGET_DB = os.getenv("TARGET_DB")
 
 ANNOTATION_DIR = "./annotations"
--- models/knowledge_graph/train_model.py
+++ models/knowledge_graph/train_model.py
@@ -18,7 +18,7 @@ load_dotenv()
 
 NEO4J_URI = os.getenv("NEO4J_URI")
 NEO4J_USER = os.getenv("NEO4J_USER")
-NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD")
+NEO4J_PASS = os.getenv("NEO4J_PASS")
 TARGET_DB = os.getenv("TARGET_DB")
 
 # ============================================================
